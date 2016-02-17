cf0319338b93ad722e70a5f06cfe8dfc850468df
jgh33 <jgh@jghdeMac-Pro.local>
2016-02-17 15:59:48 +0800

    重力完成，加上边缘限制后出现bug

--- Gemini/Gemini/GameScene.swift
+++ Gemini/Gemini/GameScene.swift
@@ -7,25 +7,81 @@
 //
 
 import SpriteKit
+import CoreMotion
+let ww = UIScreen.mainScreen().bounds.width
+let hh = UIScreen.mainScreen().bounds.height
 
 class GameScene: SKScene {
+    
     var firstHero:SKSpriteNode!
     var firstHeroCKPosition:CGPoint!
+    
+    var secondHero:SKSpriteNode!
+    var speedX:UIAccelerationValue=0
+    var speedY:UIAccelerationValue=0
+    var motionManager = CMMotionManager()
     
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
         let planeTexture = SKTexture(imageNamed: "Spaceship")
-        let plane = SKSpriteNode(texture: planeTexture)
-        plane.position = CGPointMake(size.width * 0.5, size.height * 0.5)
-        plane.setScale(0.1)
-        self.firstHero = plane
-        addChild(plane)
+        let plane1 = SKSpriteNode(texture: planeTexture)
+        plane1.setScale(0.1)
+        plane1.position = CGPointMake(size.width * 0.5, size.height * 0.5)
+        
+        self.firstHero = plane1
+        addChild(plane1)
+        
+        
+
+        let plane2 = SKSpriteNode(texture: planeTexture)
+        plane2.position = CGPointMake(size.width * 0.5, size.height * 0.3)
+        plane2.setScale(0.1)
+        self.secondHero = plane2
+        addChild(plane2)
+        
+        motionManager.accelerometerUpdateInterval = 1/60
+        
+        if(motionManager.accelerometerAvailable)
+        {
+            let queue = NSOperationQueue.currentQueue()
+            motionManager.startAccelerometerUpdatesToQueue(queue!, withHandler: {(accelerometerData : CMAccelerometerData?, error : NSError?) in
+                
+                //动态设置第二战机位置
+                self.speedX += accelerometerData!.acceleration.x
+                self.speedY +=  accelerometerData!.acceleration.y
+                var posX=self.secondHero.position.x + CGFloat(self.speedX)
+                var posY=self.secondHero.position.y - CGFloat(self.speedY)
+                
+                //碰到边框后的反弹处理
+                if posX < 0 {
+                    posX = 0
+                    self.speedX = 0
+                }else if (posX > ww) {
+                    posX = ww
+                    self.speedX = 0
+                }
+                
+                if ( posY < 0 ){
+                    posY = 0
+                    //碰到上面的边框不反弹
+                    self.speedY = 0
+                } else if posY > hh{
+                    posY = hh
+                    //碰到下面的边框以1.5倍的速度反弹
+                    self.speedY = 0
+                }
+                self.secondHero.position = CGPointMake(posX, posY)
+                
+            })
+        }
         
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
        /* Called when a touch begins */
         print("began")
+        print("\(ww)")
+        print("\(hh)")
         self.firstHeroCKPosition = touches.first!.locationInNode(self)
 
         
@@ -38,9 +94,30 @@ class GameScene: SKScene {
         
         let dx = currentTouch.x - self.firstHeroCKPosition.x
         let dy = currentTouch.y - self.firstHeroCKPosition.y
-        self.firstHero.position.x += dx
-        self.firstHero.position.y += dy
+        
+        if((self.firstHero.position.x + dx) < 0){
+            self.firstHero.position.x = 0
+        }else if ((self.firstHero.position.x + dx) > ww){
+            self.firstHero.position.x = ww
+        }else{
+            self.firstHero.position.x += dx
+        }
+        
+        
+        if((self.firstHero.position.y + dy) < 0){
+            self.firstHero.position.y = 0
+        }else if ((self.firstHero.position.y + dy) > hh){
+            self.firstHero.position.y = hh
+        }else{
+            self.firstHero.position.y += dy
+        }
+        
+        
+//        self.firstHero.position.x += dx
+//        self.firstHero.position.y += dy
         self.firstHeroCKPosition = currentTouch
+        
+        print(self.firstHero.position)
         
     }
    
